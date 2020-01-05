bd95256c7227467c93e84cc67f776e64bf52b3d5
Will Johnston <wijohnst@gmail.com>
2020-01-04 19:30:33 -0500

    added to index.pug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  const heading = 'Restaurant Manager';
+  const year = new moment(Date.now()).format('YYYY'); 
+  const foot = `${year} - Electric Lunch Lady Land, llc.`
+  res.render('index', {heading, foot});
 });
 
 module.exports = router;
+ 
